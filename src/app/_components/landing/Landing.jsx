4f9377c7da097c1b6ef8029cc5d786f78a602e1e
israrahmedjan <123giIsrar456>
2025-04-26 22:25:32 +0500

    test

--- src/app/_components/landing/Landing.jsx
+++ src/app/_components/landing/Landing.jsx
@@ -316,9 +316,9 @@ function Landing() {
 
                 <div className='flex text-base flex-col md:flex-row  justify-between items-center gap-2'>
                   <button className="px-4 py-2 md:px-4 md:py-2 bg-primary hover:bg-secondary text-white rounded-full  transition">
-                    {homeData?.Row4listing.btnLabel || "Get it More"}
+                    {homeData?.Row4Listing.btnLabel || "Get it More"}
                   </button>
-                  <Link href= {homeData?.Row4listing.btnAction || "/gosite"} className='flex'><h2 className='font-normal text-primary hover:text-secondary md:text-[18px]'>Learn more about our app</h2><ArrowRight /></Link>
+                  <Link href= {homeData?.Row4Listing.btnAction || "/gosite"} className='flex'><h2 className='font-normal text-primary hover:text-secondary md:text-[18px]'>Learn more about our app</h2><ArrowRight /></Link>
                 </div>
               </div>
 
