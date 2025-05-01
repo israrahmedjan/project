17a2df4d5aa103fcb0772e075c952cc2d9354253
israrahmedjan <123giIsrar456>
2025-05-01 23:04:07 +0500

    osdf

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -66,7 +66,6 @@ async function getHomeSeo() {
             headers: {
               Authorization: `Bearer ${token}`,
             },
-            cache: 'no-store',
             // next: {
             //   revalidate: 60, // Revalidates the page every 60 seconds
             // },
@@ -113,7 +112,6 @@ async function getUser1() {
             headers: {
               Authorization: `Bearer ${token}`,
             },
-            cache: 'no-store',
           }
         );
 
